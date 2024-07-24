877a2f5029d27e9b3c06884c7b699c18caeea383
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-24 22:48:53 +0200

    aggiunta sezione per comunicare vincita o perdita ma qualcosa non funziona, amen (':

--- js/script.js
+++ js/script.js
@@ -20,13 +20,16 @@ playBtn.addEventListener('click', function () {
     const bomba = [];
     console.log('bombe', bomba);
 
+    // aggiungo la costante per dichiarare quante sono le bombe
+    const numeroBombe = 16
+
     /* 
     creo un ciclo INDEFINITO per due motivi:
         1. perché la generazione dei numeri random potrebbe creare doppioni e non li voglio
         2. perché se crea doppioni devo stare nel ciclo finché non genera tutti numeri unici 
     */
 
-    while (bomba.length < 16) {
+    while (bomba.length < numeroBombe) {
         // richiamo la funzione per generare i numeri, gli passo i parametri
         const randomNumber = getRandomNumber(1, squaregrid);
 
@@ -58,18 +61,46 @@ playBtn.addEventListener('click', function () {
         square.addEventListener('click', function () {
             this.classList.add('squareSelected')
 
-            // aggiungo una variabile per rilevare il click specifico
-            const squareNumber = parseInt(this.innerText)
-            // se l'array, include il numero del quadrato...
-            if (bomba.includes(squareNumber)) {
-                console.log('è una bomba')
-                // ...allora aggiungo la classe bomba
-                this.classList.add('squareBomb')
+            // aggiungo le variabili per i quadrati con bombe o senza
+            const squareWithBomba = document.querySelectorAll('.squareBomb');
+            const squareWOBomba = document.querySelectorAll('.squareSelected');
+
+            // se i quadrati con le bombe sono 0, E, i quadrati senza bombe sono < del click - le bombe
+            if (squareWithBomba.length == 0
+                &&
+                squareWOBomba.length < squaregrid - numeroBombe) {
+                // allora gioca
+
+                // aggiungo una variabile per rilevare il click specifico
+                const squareNumber = parseInt(this.innerText)
+
+                // se l'array, include il numero del quadrato...
+                if (bomba.includes(squareNumber)) {
+                    console.log('è una bomba');
+
+                    // ...allora aggiungo la classe bomba
+                    this.classList.add('squareBomb');
+
+                    //aggiungo il messaggio per concludere il gioco
+                    alert('BLOOM! Hai perso, il tuo punteggio è: ' + squareWOBomba.lenght);
+                }
+                // altrimenti cliked easy peasy
+                else {
+                    console.log('non è una bomba')
+
+                    // se tutte le celle senza bomba sono state cliccate
+                    if ((squareWOBomba.length + 1) == squaregrid - numeroBombe) {
+
+                        // aggiungo il messaggio per comunicare la vincita
+                        alert('Hai vinto, il tuo punteggio è: ' + (squareWOBomba.lenght + 1));
+                    }
+                };
             }
-            // altrimenti cliked easy peasy
+            // altrimenti il gioco è concluso
             else {
-                console.log('non è una bomba')
+                alert('il gioco è concluso')
             };
+
         });
 
     };
@@ -79,4 +110,4 @@ playBtn.addEventListener('click', function () {
 // funziona per generare un numero random
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};

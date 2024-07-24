15285a43708437809d0e4b5a97080cca18af8a2e
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-24 19:52:03 +0200

    inizio scrittura js per bombe: - funzione per generare numeri random - ciclo per generarle entro il numero di quadrati

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
-/*
-
-per creare elemento html
-    1. document.createELement --> questo consente di poterci cliccare sopra in modo più facile
-    2. stringa con innerHTML
-*/
-
-// dichiaro il bottone per far partire il gioco
 const playBtn = document.getElementById('gioca');
 
-// ascolto il click sul bottone
 playBtn.addEventListener('click', function () {
-    console.log('Ho cliccato');
 
-    // dichiaro il container
     const container = document.getElementById('squareContainer')
 
-    //aggiungere le opzioni del gioco
     const opzioniGioco = document.getElementById('opzioniGioco')
-    // controllo che il valore di opzioni gioco sia corretto
     console.log(opzioniGioco.value)
 
-    // aggiungo variabile per cambiare il numero di celle in base alla difficoltà
-    // quella di base è 100
     let squaregrid = 100;
 
     if (opzioniGioco.value == 'Medium') {
@@ -31,18 +16,25 @@ playBtn.addEventListener('click', function () {
         squaregrid = 49;
     };
 
-    // per far sì che ogni volta che clicco, non mi si duplichino i quadrati, devo svuotare il container
+    // creo le bombe
+    const bomba = []
+    // creo un ciclo per generare le 16 bombe
+    for (let i = 0; i <= 16; i++) {
+
+        // richiamo la funzione per generare i numeri, gli passo i parametri
+        const randomNumber = getRandomNumber(1, squaregrid);
+        console.log('randomNumber', randomNumber, typeof randomNumber);
+
+        // metto la bomba dentro l'array
+        bomba.push(randomNumber);
+
+    };
+
     container.innerHTML = ''
 
-    // genero un ciclo che mi scriva 100 elementi al clicl
     for (let i = 1; i <= squaregrid; i++) {
-        console.log(i);
 
-        // creo l'elemento in html
         const square = document.createElement('div');
-        // metto il testo dentro il quadrato
-        square.innerHTML = i;
-        // metto il quadrato dentro il suo container
         squareContainer.append(square);
 
         if (squaregrid == 100) {
@@ -55,14 +47,14 @@ playBtn.addEventListener('click', function () {
             square.classList.add('difficile')
         };
 
-        //ascolto il click
         square.addEventListener('click', function () {
-
-            // aggiungo la classe del quadrato selezionato
             this.classList.add('squareSelected')
-            console.log(this.innerHTML);
         });
 
     };
 
 });
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
